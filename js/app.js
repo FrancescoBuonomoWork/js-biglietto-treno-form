43bfb12d32066f6e341eaed13dbc186aff6a0f40
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-10-05 15:41:40 +0200

    add try to select element

--- js/app.js
+++ js/app.js
@@ -1,20 +1,52 @@
-console.log('calcolatore');
+// console.log('calcolatore');
 // - stabilire in una variabile il prezzo al km e gli sconti tramite delle const 
 const PREZZO_PER_KM = 0.21;
 const SCONTO_OVER_65 = 40;
 const SCONTO_MINORENNI = 20;
+
 // prendo il pulsante dal DOM 
 const btnDOMElement = document.getElementById('btn-calc')
-console.log(btnDOMElement)
+// console.log(btnDOMElement)
 // - chiedere all' utente i chilometri che vuole percorrere tramite un input quindi prendendolo dal DOM
 const inputDOMElement = document.getElementById('expected-km');
-console.log('inputDOMElement');
+// console.log('inputDOMElement');
 // prendo il  prezzo dal DOM 
 const prezzoDOMElement = document.querySelector('.price');
-console.log(prezzoDOMElement);
+// console.log(prezzoDOMElement);
 // prendo lo sconto dal DOM 
 const selDOMElement = document.getElementById('discount');
 // al bottone aggiungere un evento click con il quale immagazzineremo i valori esposti dall'utente e calcoleremo il prezzo da pagare 
 btnDOMElement.addEventListener('click', function () {
-    
+    // prendiamo dal click del bottone i km selezionati
+    const km = parseFloat(inputDOMElement.value)
+    console.log(km)
+    // calcoliamo lo sconto
+    let discount = selDOMElement.value
+    console.log(discount)
+    let discountRate = ((discount.value = 0) = SCONTO_MINORENNI )
+    console.log(discountRate)
+    if (discount.value = 0) {
+    //  let  discountRate = SCONTO_MINORENNI 
+    //         console.log(discountRate)
+     }     
+    else if (discount.value = 2) {
+    let  discountRate = SCONTO_OVER_65
+        console.log(discountRate)
+    }    
+
+
+    // if (age < 18) {
+    //     discount = totalPrice * SCONTO_MINORENNI / 100
+    //     console.log(discount)
+    //     totalPrice = totalPrice - discount
+        
+    // } else if (age > 65) {
+        
+    //     discount = totalPrice * SCONTO_OVER_65 / 100
+    //     console.log(discount)
+    //     totalPrice = totalPrice - discount
+    // }
+    // calcoliamo il prezzo totale moltiplicando i km dichiarati * il prezzo al km 
+    // const totalPrice = km * PREZZO_PER_KM
+    // console.log(totalPrice)
 })
