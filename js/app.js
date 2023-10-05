dd672a1598aadc1a6fca12f4136a802240d08804
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-10-05 16:11:16 +0200

    commit della disperazione con questa select

--- js/app.js
+++ js/app.js
@@ -20,33 +20,35 @@ btnDOMElement.addEventListener('click', function () {
     // prendiamo dal click del bottone i km selezionati
     const km = parseFloat(inputDOMElement.value)
     console.log(km)
+    // calcoliamo il prezzo totale moltiplicando i km dichiarati * il prezzo al km 
+    const totalPrice = km * PREZZO_PER_KM
+    console.log(totalPrice)
     // calcoliamo lo sconto
     let discount = selDOMElement.value
     console.log(discount)
-    let discountRate = ((discount.value = 0) = SCONTO_MINORENNI )
-    console.log(discountRate)
-    if (discount.value = 0) {
-    //  let  discountRate = SCONTO_MINORENNI 
-    //         console.log(discountRate)
-     }     
-    else if (discount.value = 2) {
-    let  discountRate = SCONTO_OVER_65
+    // let discountRate = ((discount.value = 0) = SCONTO_MINORENNI )
+
+    let discountRate = 0;
+    if (discount.value == 0) {
+        discountRate = totalPrice * SCONTO_MINORENNI / 100
+        
         console.log(discountRate)
-    }    
+    }
+    else if (discount.value == 2) {
+        discountRate = SCONTO_OVER_65
+        console.log(discountRate)
+    }
 
-
+    console.log(discountRate);
     // if (age < 18) {
     //     discount = totalPrice * SCONTO_MINORENNI / 100
     //     console.log(discount)
     //     totalPrice = totalPrice - discount
-        
+
     // } else if (age > 65) {
-        
+
     //     discount = totalPrice * SCONTO_OVER_65 / 100
     //     console.log(discount)
     //     totalPrice = totalPrice - discount
     // }
-    // calcoliamo il prezzo totale moltiplicando i km dichiarati * il prezzo al km 
-    // const totalPrice = km * PREZZO_PER_KM
-    // console.log(totalPrice)
 })
